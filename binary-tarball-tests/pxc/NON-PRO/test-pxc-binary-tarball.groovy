77f8cd4936ff5e957a861aa018405cff447351c0
Anderson Nogueira <anderson.nogueira@percona.com>
2026-08-19 17:22:17 +0000

    PKG-1453: point validation clones at the validation branch

--- binary-tarball-tests/pxc/NON-PRO/test-pxc-binary-tarball.groovy
+++ binary-tarball-tests/pxc/NON-PRO/test-pxc-binary-tarball.groovy
@@ -43,7 +43,7 @@ pipeline {
                                 fi
                                 rm -rf package-testing
                                 sudo apt install -y git wget tar socat
-                                git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                                git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                 cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                 wget -q ${TARBALL_LINK}${TARBALL_NAME}
                                 df -mh
@@ -76,7 +76,7 @@ pipeline {
                                 fi
                                 rm -rf package-testing
                                 sudo apt install -y git wget tar socat
-                                git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                                git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                 cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                 wget -q ${TARBALL_LINK}${TARBALL_NAME}
                                 ./run.sh || true
@@ -108,7 +108,7 @@ pipeline {
                                 fi
                                 rm -rf package-testing
                                 sudo apt install -y git wget tar socat
-                                git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                                git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                 cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                 wget -q ${TARBALL_LINK}${TARBALL_NAME}
                                 ./run.sh || true
@@ -140,7 +140,7 @@ pipeline {
                                 fi
                                 rm -rf package-testing
                                 sudo apt install -y git wget tar
-                                git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                                git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                 cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                 wget -q ${TARBALL_LINK}${TARBALL_NAME}
                                 ./run.sh || true
@@ -172,7 +172,7 @@ pipeline {
                                 fi
                                 rm -rf package-testing
                                 sudo apt install -y git wget tar
-                                git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                                git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                 cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                 wget -q ${TARBALL_LINK}${TARBALL_NAME}
                                 ./run.sh || true
@@ -204,7 +204,7 @@ pipeline {
                                 fi
                                 rm -rf package-testing
                                 sudo apt install -y git wget tar
-                                git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                                git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                 cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                 wget -q ${TARBALL_LINK}${TARBALL_NAME}
                                 ./run.sh || true
@@ -236,7 +236,7 @@ pipeline {
                                fi
                                rm -rf package-testing
                                sudo yum install -y git wget tar
-                               git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                               git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                wget -q ${TARBALL_LINK}${TARBALL_NAME}
                               ./run.sh || true
@@ -269,7 +269,7 @@ pipeline {
                                 fi
                                 rm -rf package-testing
                                 sudo yum install -y git wget tar
-                                git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                                git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                 cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                 wget -q "${TARBALL_LINK}${TARBALL_NAME}"
                                ./run.sh || true
@@ -301,7 +301,7 @@ pipeline {
                                 fi
                                 rm -rf package-testing
                                 sudo yum install -y git wget tar
-                                git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                                git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                 cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                 wget -q "${TARBALL_LINK}${TARBALL_NAME}"
                                ./run.sh || true
@@ -333,7 +333,7 @@ pipeline {
                                 fi
                                 rm -rf package-testing
                                 sudo yum install -y git wget tar
-                                git clone https://github.com/kaushikpuneet07/package-testing.git --branch add-pxc97-dock --depth 1
+                                git clone https://github.com/nogueiraanderson/package-testing.git --branch pkg1453-sst-auth-gate-validation --depth 1
                                 cd package-testing/binary-tarball-tests/pxc/NON-PRO
                                 wget -q "${TARBALL_LINK}${TARBALL_NAME}"
                                ./run.sh || true
